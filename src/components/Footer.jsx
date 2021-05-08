62e3c3a10df6321af814b4b55fef481097386a59
loot01 <stargaming654@gmail.com>
2021-05-08 14:19:38 +0000

    changed footer to be fixed on the bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
 const FooterWrapper = styled.div`
-  position: absolute;
-  top: 90%;
   text-align: center;
   font-size: 14px;
   font-weight: 500;
   color: #a9a9a9;
   left: 0;
   right: 0;
+  margin-top: 80px;
   margin-right: auto;
   margin-left: auto;
+  margin-bottom: 10px;
   width: max-content;
 `;
 
